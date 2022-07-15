f0c6134c4d3be3fad584379e2dda4809a720fab9
JTDoe <Jordendoerflinger@gmail.com>
2022-07-14 20:48:47 -0500

    fixed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,7 @@
 import React from "react";
 
-// export function Button(props) {
-//   const { label = "Default", handleClick = () => console.log("Default") } =
-//     props;
-//   return <button onClick={handleClick}>{label}</button>;
-// }
-
-export class ButtonClass extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { label = "Default", handleClick = () => console.log("Default") } =
-      this.props;
-
-    return <button onClick={handleClick}>{label}</button>;
-  }
+export function Button(props) {
+  const { label = "Default", handleClick = () => console.log("Default") } =
+    props;
+  return <button onClick={handleClick}>{label}</button>;
 }
